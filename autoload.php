34744ca0619c2687cf393c382df5cd346fb2a909
oxynade <sergey.poskachey@oxynade.com>
2015-12-31 10:00:22 +0100

    Update autoload.php

--- autoload.php
+++ autoload.php
@@ -4,4 +4,7 @@ $autoload = __DIR__ . '/vendor/autoload.php';
 if (!file_exists($autoload)) {
     throw new \RuntimeException('Missing composer autoload: ' . $autoload);
 }
+
+// return
+
 return require_once $autoload;
